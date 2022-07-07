ab4c0155f1934d45f12ec491e105e8cdd48e0bb5
”David <brayzgunov28@gmail.com>
2022-07-07 12:29:46 +0600

    вертска окей

--- src/components/block/Block.jsx
+++ src/components/block/Block.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import styles from './block.module.css'
 import logo from "./logo.svg"
 
-function Block() {
+function Block({text}) {
   return (
    <section className={styles.block}>
      <section className={styles.blockLeft}>
        <img src={logo} alt="" className={styles.logo} />
        <div className={styles.info}>
          <h3> Analyzing User Data</h3>
+         {/* <div className={styles.test}>
+         Who are you developing for? How will they use your design? Building user personas can answer these.
+         </div> */}
+         
          <p>Who are you developing for? How will they use your design? Building user personas can answer these.</p>
+         
        </div>
      </section>
 
      <section className={styles.doubleButton}>
-       
+       <p>6 videos</p>
+       <p>4.5 hours</p>
      </section>
 
    </section>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,7 @@ function Header() {
   return (
    <section className={styles.header}>
      <h2> What you’ll learn</h2>
-     {/* <div className={styles.test} > Create beautiful webflow sites with flowbase. Clone for
-free and get started building a site today.</div> */}
+   
      <p>Create beautiful webflow sites with flowbase. Clone for
 free and get started building a site today.</p>
    </section>
